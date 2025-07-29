c8cc3fcb32adf803658a4535842696105abfe70a
AdiwenoRS <adiwenors@gmail.com>
2025-07-29 17:00:38 +0700

    Generic view

--- djangoTutorial/polls/views.py
+++ djangoTutorial/polls/views.py
@@ -1,18 +1,62 @@
-from django.shortcuts import render, HttpResponse
-from .models import Question
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F
+from django.views import generic
+
+from .models import Question, Choice
 
 # Create your views here.
+
+'''
+# Before refactoring (this is function-based views)
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse("Latest questions: " + output)
+    context = { # This is a dictionary that will be passed to the template
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("you're looking at question %s." % question_id) 
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+'''
 
+# Refactored views using class-based views
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+        
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    
+# Function-based view for voting
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question_id = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question_id.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question_id,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing with POST data.
+        # This prevents data from being posted twice if a user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id.id,)))
